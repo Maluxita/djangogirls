f86b5784ae0fe1b6632b6ba8d7e14bea5864ff32
Malu <malu.geom@gmail.com>
2017-04-01 17:48:55 -0600

    corregido el path de staticos

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
